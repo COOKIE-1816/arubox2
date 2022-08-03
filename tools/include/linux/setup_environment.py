a81157a79a37d2242fc9f67cda0b5bf351aaaefb
COOKIE-1816 <93037688+COOKIE-1816@users.noreply.github.com>
2022-08-03 14:06:07 +0200

    Add files via upload

--- tools/include/linux/setup_environment.py
+++ tools/include/linux/setup_environment.py
@@ -2,3 +2,8 @@ def boardSelector():
     isMainBoard = input("Are you editing configuration for manager board? [y/N]")
     isMainBoard = not isMainBoard == "Y" and not isMainBoard == "y"
 
+def listf():
+    if not isMainBoard:
+        listfile = open("./include/setup_variables_manager.txt", "r")
+        print(listfile.read())
+        listfile.close()
