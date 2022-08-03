22b48aeb0079df6d052e3b16637c770fc41b1788
COOKIE-1816 <93037688+COOKIE-1816@users.noreply.github.com>
2022-08-03 14:15:30 +0200

    Add files via upload

--- tools/include/linux/setup_environment.py
+++ tools/include/linux/setup_environment.py
@@ -5,5 +5,8 @@ def boardSelector():
 def listf():
     if not isMainBoard:
         listfile = open("./include/setup_variables_manager.txt", "r")
-        print(listfile.read())
-        listfile.close()
+    else:
+        listfile = open("./include/setup_variables_board.txt", "r")
+        
+    print(listfile.read())
+    listfile.close()
---
+++ tools/setup_environment.sh
@@ -0,0 +1,1 @@
+python ./include/linux/setup_environment
