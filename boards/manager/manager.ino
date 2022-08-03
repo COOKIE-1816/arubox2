7f8c49aed8d35b52567e78272ed6fca3c0fd86ae
COOKIE-1816 <93037688+COOKIE-1816@users.noreply.github.com>
2022-08-03 17:14:32 +0200

    Add files via upload

--- boards/manager/manager.ino
+++ boards/manager/manager.ino
@@ -5,64 +5,131 @@
 
 int attemp = 0;
 int bps = 9600;
+int boardStatusIndicator;
+int seconds;
+int minutes;
+int hours;
+int board;
+int signalizator;
+int exceptedBoardStatus;
 
+bool isActive() {
+    // return digitalRead(boardStatusIndicator) == HIGH;
+    if(digitalRead(boardStatusIndicator) == HIGH) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+/*
 class Monitoring {
     public:
         bool isActive() {
-            return bool digitalRead(boardStatusIndicator) == HIGH;
-        }
-}
-
-class ManagerBoard {
-    void readConfig() {};
-    void startSetup() {
-        Serial.begin(bps);
-        Serial1.begin(bps);
-        Serial2.begin(bps);
-    };
-}
-
-class Power {
-    void sigtermf() {
-        digitalWrite(signalizator, HIGH);
-        delay(2500);
-        digitalWrite(signalizator, LOW);
-
-        for(int i = 0; i < 301; i++) {
-            if(Monitoring::isActive() == false) {
-                Power::power(false);                                                            // Power off
+            // return digitalRead(boardStatusIndicator) == HIGH;
+            if(digitalRead(boardStatusIndicator) == HIGH) {
+                return true;
+            } else {
+                return false;
             }
         }
+};
+*/
 
-        Power::power(false);                                                                    // Power off
-    };
+class ManagerBoard {
+    /*
+    public:
+        void readConfig() {};
+        void startSetup() {
+            Serial.begin(bps);
+        };
+    */
+};
 
-    void power(bool power_) {
-        if(power_) {
-            digitalWrite(board, HIGH);
-        } else {
-            digitalWrite(board, LOW);
+class Clock {
+    public:
+        int sec = 0;
+        int min = 0;
+        int hrs = 0;
+
+        void inform() {
+            analogWrite(seconds, Clock::sec);
+            analogWrite(minutes, Clock::min);
+            analogWrite(hours, Clock::hrs);
+        };
+
+        void reset() {
+            Clock::sec = 0;
+            Clock::min = 0;
+            Clock::hrs = 0;
+        };
+
+        void increase(int seconds) {
+            if(Clock::sec == 59) {  //ok
+                if(Clock::min == 59) { //ok
+                    if(Clock::hrs == 23) {
+                        Clock::reset();
+                    } else {
+                        Clock::hrs = Clock::hrs + 1;
+                        Clock::min = 0;
+                        Clock::sec = 0;
+                    }
+                } else {
+                    Clock::min = Clock::min + 1;
+                    Clock::sec = 0;
+                }
+            } else {
+                Clock::sec = Clock::sec + 1;
+            }
+        };
+};
+
+class Power {
+    public:
+        void sigtermf() {
+            digitalWrite(signalizator, HIGH);
+            delay(2500);
+            digitalWrite(signalizator, LOW);
+
+            for(int i = 0; i < 301; i++) {
+                if(!isActive()) {
+                    Power::power(false);                                                            // Power off
+                }
+            }
+
+            Power::power(false);                                                                    // Power off
+        };
+
+        void power(bool power_) {
+            if(power_) {
+                digitalWrite(board, HIGH);
+            } else {
+                digitalWrite(board, LOW);
+            }
+        };
+
+        void reset(bool force) {
+            if(force) {
+                Power::power(false);
+            } else {
+                Power::sigtermf();
+            }
+
+            delay(500);
+            Power::power(true);
         }
-
-        return Monitoring::isActive();
-    };
-
-    void reset(bool force) {
-        if(force) {
-            Power::power(false);
-        } else {
-            Power::sigtermf();
-        }
-
-        delay(500);
-        Power::power(true);
-    }
-}
+};
 
 void setup() {
-    ManagerBoard::startSetup();
+    Serial.begin(bps);
 }
 
 void loop() {
-    if(Monitoring::isActive() == (exceptedBoardStatus == HIGH)) {}
+    /*
+    if(isActive() == exceptedBoardStatus == HIGH) {
+        Clock::increase(1);
+    } else {
+        digitalWrite(board, exceptedBoardStatus);
+    }
+    */
 }
