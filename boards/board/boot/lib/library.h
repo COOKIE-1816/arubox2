adc77e52b215fa47a1771517e023b63477e8de7c
COOKIE <93037688+COOKIE-1816@users.noreply.github.com>
2022-08-05 09:44:57 +0200

    Add files via upload

---
+++ boards/board/boot/lib/library.h
@@ -0,0 +1,18 @@
+#ifndef Library_h
+#define Library_h
+
+#include "Arduino.h"
+
+/*
+class Morse {
+    public:
+        Morse(int pin);
+        void dot();
+        void dash();
+    private:
+        int _pin;
+
+};
+*/
+
+#endif
